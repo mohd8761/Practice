a4249d2254927a0573fd91ef9ab7dea76b8af3f5
erahmhd <rahmathullah.mohd@ericsson.com>
2017-12-06 22:30:31 -0500

    removed conflicting

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,11 +2,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-<<<<<<< Updated upstream
+
         System.out.println("Hello, Worlddddddhhhh1112223333");
-=======
-        System.out.println("Hello, Worlddddddhhhh9999");
->>>>>>> Stashed changes
+
     }
 
 }
